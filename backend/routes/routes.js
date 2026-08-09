09c4457c673be087ffb0d83cd416597a1180da05
Aman Dobhal <dobhalaman71@gmail.com>
2026-08-09 10:48:29 +0530

    QR String Generated Successfully in the POST Route

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,14 +1,20 @@
 import express from 'express';
+import QRcode from 'qrcode';
 
 const router = express.Router();
 
-router.post('/generateQR' , (req , res) => {
+router.post('/generateQR' , async(req , res) => {
     
     const data = req.body;
 
+    console.log(data.content);
+
+    const qrcode = await QRcode.toDataURL(data.content);
+
+
     res.status(201).json({
-        "message":"Data Received Successfully",
-        "data": data
+        "message":"QR Generated Successfully",
+        "qrCode": qrcode
     });
 
 })
